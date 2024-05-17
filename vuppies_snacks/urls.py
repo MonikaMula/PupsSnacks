34092650a2c583e242269fb05d6dc14e618198fa
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2024-05-17 02:05:23 +0100

    home app and templates

--- vuppies_snacks/urls.py
+++ vuppies_snacks/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include  # include its added
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # added
+    path('', include('home.urls')),
 ]
