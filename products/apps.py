579f397a385e70d92663ac90dcd601fb090e1900
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2024-05-19 17:51:05 +0100

    product errors

--- products/apps.py
+++ products/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
-class ProductsConfig(AppConfig):
-    name = 'products'
-
 #class ProductsConfig(AppConfig):
-    #default_auto_field = 'django.db.models.BigAutoField'
-    #name = 'products'
+  #  name = 'products'
+
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'products'
     
--- vuppies_snacks/urls.py
+++ vuppies_snacks/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # added
+    path('products/', include('products.urls')), 
     path('', include('home.urls')),
 ]
 
